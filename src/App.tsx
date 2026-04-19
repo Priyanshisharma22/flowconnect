67b74dcc5e96475be6d003a40c7de50bc161fdf0
ankitt2005 <ankitbiswal144@gmail.com>
2026-04-19 16:27:57 +0530

    fix: Add missing React imports to resolve CI JSX namespace errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
